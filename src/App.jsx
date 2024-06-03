97847287a0405cc982cc100126c9262da814ae43
samikshya <samikshyakharel405@gmail.com>
2024-06-03 21:43:33 +1000

    router path given

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Login from "./page/Login";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./page/SignUp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 };
 
 export default App;
