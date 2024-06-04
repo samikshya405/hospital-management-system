5f6b247448402d21005d3532403d00c51721ae54
samikshya <samikshyakharel405@gmail.com>
2024-06-04 17:55:11 +1000

    page for billing

---
+++ src/page/billing/Billing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../component/main/MainLayout'
+
+const Billing = () => {
+  return (
+    <MainLayout title="Billing and Payment">
+
+    </MainLayout>
+  )
+}
+
+export default Billing
