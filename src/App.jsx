fc9526dedafc89e1818ced5b5b4950cd77704a58
samikshya <samikshyakharel405@gmail.com>
2024-06-04 23:57:17 +1000

    path given

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import { Route, Routes } from "react-router-dom";
 import SignUp from "./page/auth/SignUp";
 import Dashboard from "./page/dashboard/Dashboard";
 import Main from "./component/main/MainLayout";
+import PatientRegistration from "./page/patientRegistration/PatientRegistration";
+import Appointment from "./page/appointment/Appointment";
+import Billing from "./page/billing/Billing";
+import ClinicalManagement from "./page/clinical/ClinicalManagement";
+import ElectronicMedicalRecord from "./page/emr/ElectronicMedicalRecord";
+import DoctorInformation from "./page/doctors/DoctorInformation";
+import InPatient from "./page/inPatient/InPatient";
+import OutPatient from "./page/outPatient/OutPatient";
+import Patients from "./page/patients/Patients";
+import Statistics from "./page/statistics/Statistics";
+import Emergency from "./page/emergency/Emergency";
 
 const App = () => {
   return (
@@ -11,6 +22,17 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Dashboard />} />
+      <Route path="/patientRegistration" element={<PatientRegistration/>}/>
+      <Route path="/appointment" element={<Appointment/>}/>
+      <Route path="/billing" element={<Billing/>}/>
+      <Route path="/clinical" element={<ClinicalManagement/>}/>
+      <Route path="/emr" element={<ElectronicMedicalRecord/>}/>
+      <Route path="/doctors" element={<DoctorInformation/>}/>
+      <Route path="/inPatient" element={<InPatient/>}/>
+      <Route path="/outPatient" element={<OutPatient/>}/>
+      <Route path="/patients" element={<Patients/>}/>
+      <Route path="/statistic" element={<Statistics/>}/>
+      <Route path="/emergency" element={<Emergency/>}/>
     </Routes>
   );
 };
