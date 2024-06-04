3f317c12705f8337d3090e2cd776d26fcc92cfd3
samikshya <samikshyakharel405@gmail.com>
2024-06-04 17:54:29 +1000

    page for appointment

---
+++ src/page/appointment/Appointment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../component/main/MainLayout'
+
+const Appointment = () => {
+  return (
+    <MainLayout title="Appointment">
+
+    </MainLayout>
+  )
+}
+
+export default Appointment
