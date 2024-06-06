6ec43b960ff0c937ce356c2d6a34ef8f3074931f
samikshya <samikshyakharel405@gmail.com>
2024-06-06 20:32:06 +1000

    roster folder added

---
+++ src/page/rosters/Rosters.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../component/main/MainLayout'
+
+const Rosters = () => {
+  return (
+    <MainLayout title={"Rosters"}>
+
+    </MainLayout>
+  )
+}
+
+export default Rosters
