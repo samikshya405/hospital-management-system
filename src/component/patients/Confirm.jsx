f5962446698e4415d4e9dfa5dcd0cbbc4e80c4ae
samikshya <samikshyakharel405@gmail.com>
2024-06-06 18:40:10 +1000

    confirm page created for patient registration

---
+++ src/component/patients/Confirm.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Confirm = () => {
+  return <Box   py={2}>
+    
+    
+
+  </Box>;
+};
+
+export default Confirm;
