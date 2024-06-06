2ce79ccc977f6ab07db4d65706dbbaf89dd6aa02
samikshya <samikshyakharel405@gmail.com>
2024-06-06 20:32:54 +1000

    folder for staff added

---
+++ src/page/staffs/Staffs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../component/main/MainLayout'
+
+const Staffs = () => {
+  return (
+    <MainLayout title={"Staff"}>
+
+    </MainLayout>
+  )
+}
+
+export default Staffs
