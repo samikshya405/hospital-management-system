1cf3276aa80634ee898d707dd4daa6c1e5f368b6
samikshya <samikshyakharel405@gmail.com>
2024-07-16 16:20:38 +1000

    employement form added to database

--- src/component/staff/EmployeeOnboardingForm.jsx
+++ src/component/staff/EmployeeOnboardingForm.jsx
@@ -1,30 +1,156 @@
-import { Box, Button, Grid, InputLabel, Typography } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { CustomInput, CustomSelect } from "../auth/CustomInput";
-import { personalDetails, staffDetails } from "../main/dataSet";
+import { personalDetails } from "../main/dataSet";
 import { Form } from "react-router-dom";
+import { addNewEmployee, getDepartmentList } from "../../utils/axiosHelper";
 const initialState = {
   fName: "",
   mName: "",
   lName: "",
-  dob: null,
+  dob: "",
   gender: "",
-  maritalStatus: "Single",
-  occupation: "",
-  language: "",
-  religion: "",
-  nationality: "",
+  phone: 8998,
   email: "",
+  department: "",
+  jobTitle: "",
+  startDate: "",
+  employmentType: "",
 };
+const staffDetails = [
+  {
+    name: "fName",
+    label: "First Name",
+    id: "fName",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "mName",
+    label: "Middle Name",
+    id: "mName",
+    type: "text",
+    required: false,
+  },
+  {
+    name: "lName",
+    label: "Last Name",
+    id: "lName",
+    type: "text",
+    required: true,
+  },
 
+  {
+    name: "dob",
+    label: "DOB",
+    id: "dob",
+    type: "date",
+    required: true,
+  },
+  {
+    name: "gender",
+    label: "Gender",
+    id: "gender",
+    type: "select",
+    required: true,
+    option: [
+      {
+        name: "Male",
+      },
+      {
+        name: "Female",
+      },
+      {
+        name: "Other",
+      },
+    ],
+  },
+  {
+    name: "phone",
+    label: "Phone",
+    id: "phone",
+    type: "number",
+    required: true,
+  },
+  {
+    name: "email",
+    label: "Email",
+    id: "email",
+    type: "text",
+    required: true,
+  },
+];
+const jobDetails = [
+  {
+    name: "jobTitle",
+    label: "Job Title",
+    id: "jobTitle",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "startDate",
+    label: "Start Date",
+    id: "startDate",
+    type: "date",
+    required: true,
+  },
+  {
+    name: "employmentType",
+    label: "Employment Type",
+    id: "employmentType",
+    type: "select",
+    required: true,
+    option: [
+      {
+        name: "Part time",
+      },
+      {
+        name: "Full Time",
+      },
+      {
+        name: "Casual",
+      },
+    ],
+  },
+];
 const EmployeeOnboardingForm = () => {
   const [formData, setformData] = useState(initialState);
+
+  const [departmentList, setDepartmentList] = useState({
+    name: "department",
+    required: "true",
+    option: [],
+  });
+  const getDepartment = async () => {
+    const department = await getDepartmentList();
+    setDepartmentList({ ...departmentList, option: department.department });
+  };
+  useEffect(() => {
+    getDepartment();
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setformData({ ...formData, [name]: value });
+    // console.log(formData);
   };
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    const response = await addNewEmployee(formData);
+    console.log(response);
+    setformData(initialState);
+  };
   return (
     <Box
       width={"80%"}
@@ -44,8 +170,53 @@ const EmployeeOnboardingForm = () => {
                   <>
                     <InputLabel>{label}</InputLabel>
                     <CustomInput
+                      value={formData[input.name]}
                       key={input.id}
                       {...input}
+                      onChange={handleChange}
+                    />
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <InputLabel>{label}</InputLabel>
+                    <CustomSelect
+                      input={input}
+                      value={formData[input.name]}
+                      onChange={handleChange}
+                    />
+                  </>
+                )}
+              </Grid>
+            );
+          })}
+          <Grid item xs={12} md={6}>
+            <InputLabel>Department</InputLabel>
+            <Select
+              value={formData.department}
+              name="department"
+              fullWidth
+              onChange={handleChange}
+            >
+              {departmentList.option?.map((item) => {
+                return (
+                  <MenuItem key={item._id} value={item.department}>
+                    {item.department}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Grid>
+          {jobDetails.map(({ label, ...input }, i) => {
+            return (
+              <Grid item xs={12} md={6} key={input.id + i}>
+                {input.type !== "select" ? (
+                  <>
+                    <InputLabel>{label}</InputLabel>
+                    <CustomInput
+                      key={input.id}
+                      {...input}
+                      value={formData[input.name]}
                       onChange={handleChange}
                     />
                   </>
